70ce04bf31d40f9358d1eb89f4095f48f176fd25
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-16 13:18:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,9 +23,7 @@ function maybeTrue() {
 }
 
 function doWhileLoop(array) {
-  // var theArray = [...array]
   do {
-    // theArray = theArray.slice(-1)
     array = array.slice(-1)
   } while (array.length > 0 && maybeTrue())
   return array
