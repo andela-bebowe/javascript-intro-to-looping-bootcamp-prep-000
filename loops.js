117923cf9ebb9321c52dc48a06f3b593bd0e873f
Blessing Ebowe <blessingebowe@gmail.com>
2017-10-16 13:18:42 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(array) {
+  var theArray = [...array]
+  for (var i = 0; i < 25; i++) {
+    if (i == 1) {
+      theArray = [...theArray, "I am 1 strange loop."]
+    } else {
+      theArray = [...theArray, `I am ${i} strange loops.`]
+    }
+  }
+  return theArray
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n)
+    n -= 1
+  }
+  return 'done'
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+  do {
+    array = array.slice(-1)
+  } while (array.length > 0 && maybeTrue())
+  return array
+}
